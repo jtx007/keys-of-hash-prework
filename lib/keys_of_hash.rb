cb7c3706535c19cd257b2df48cfc12b8bec999ff
James Thomas <jamesjacobthomas7@gmail.com>
2018-05-31 19:29:51 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -6,8 +6,9 @@ class Hash
     key_array = []
     self.each do |key, value|
       arguments.each do |argument|
-      if value == argument
-        key_array << key
+        if value == argument
+          key_array << key
+        end
       end
     end
     key_array
